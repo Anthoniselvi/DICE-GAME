8c612431fe55b05dfd90676cc7efacc8c3975aeb
Anthoniselvi <110671748+Anthoniselvi@users.noreply.github.com>
2022-09-03 14:46:50 +0530

    Add files via upload
    updated javascript

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let player1Score = 0
 let player2Score = 0
 let player3Score = 0
 
+let player1Turn = false
+let player2Turn = false
+let player3Turn = false
+
 const player1Dice = document.getElementById("player1Dice")
 const player2Dice = document.getElementById("player2Dice")
 const player3Dice = document.getElementById("player3Dice")
@@ -19,8 +23,9 @@ function showResetButton(){
 
 rollBtn.addEventListener ('click', function(){
     const randomNumber = Math.floor (Math.random()*6)+1
-    
-    if (player1Turn=true){
+       
+    if (player1Turn === true) {
+        
         player1Score += randomNumber
         player1Scoreboard.textContent = player1Score
         player1Dice.textContent = randomNumber
@@ -28,7 +33,9 @@ rollBtn.addEventListener ('click', function(){
         player2Dice.classList.add("active")
         message.textContent = "Player 2 Turn"
     } 
-    else if (player2Turn=true){
+
+    else if (player2Turn === true) {
+
         player2Score += randomNumber
         player2Scoreboard.textContent = player2Score
         player2Dice.textContent = randomNumber
@@ -36,17 +43,18 @@ rollBtn.addEventListener ('click', function(){
         player3Dice.classList.add("active")
         message.textContent = "Player 3 Turn"
     }  
-    else if (player3Turn=true) {
+
+    else if (player3Turn === true) {
+
         player3Score += randomNumber
         player3Scoreboard.textContent = player3Score
         player3Dice.textContent = randomNumber
         player3Dice.classList.remove("active")
         player1Dice.classList.add("active")
         message.textContent = "Player 1 Turn"
-    }
-    })
+    }  
 
-    
+  
     if (player1Score >= 20){
         message.textContent = "Player 1 Won 🏆"
         showResetButton()
@@ -56,9 +64,10 @@ rollBtn.addEventListener ('click', function(){
     } else if (player3Score >=20){
         message.textContent = "Player 3 Won 🏆"
         showResetButton()
-
+    }
+    
     player1Turn = !player1Turn
-}
+})
 
 resetBtn.addEventListener('click', function(){
     reset()
@@ -69,23 +78,50 @@ function reset(){
     player1Score = 0
     player2Score = 0
     player3Score = 0
-    
+
     player1Scoreboard.textContent = 0
     player2Scoreboard.textContent = 0
     player3Scoreboard.textContent = 0
+
     player1Dice.textContent = "-"
     player2Dice.textContent = "-"
     player3Dice.textContent = "-"
-    message.textContent = "Player 1 Turn"
+
+   if (player1Turn === True) {
     
-    resetBtn.style.display = "none"
-    rollBtn.style.display = "block"
+        message.textContent = "Player 1 Turn"
+        resetBtn.style.display = "none"
+        rollBtn.style.display = "block"
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+   }
+    else if (player2Turn === True) {
 
-    player2Dice.classList.remove("active")
-    player3Dice.classList.add("active")
+        message.textContent = "player 2 Turn"
+        resetBtn.style.display = "none"
+        rollBtn.style.display = "block"
+        player3Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+    }
+    else if (player3Turn === True) {
 
-    player3Dice.classList.remove("active")
-    player1Dice.classList.add("active")
+        message.textContent = "Player 3 Turn"
+        resetBtn.style.display = "none"
+        rollBtn.style.display = "block"
+        player1Dice.classList.remove("active")
+        player3Dice.classList.add("active")
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
 
 
